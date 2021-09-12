0ede626da0abf30e6136781ce4ea070ad077e571
jrc03c <jrc03c@pm.me>
2021-09-12 12:05:51 -0500

    ...

--- tests/get-dirs-deep-sync.test.js
+++ tests/get-dirs-deep-sync.test.js
@@ -1,3 +1,8 @@
-require("./setup-and-teardown.js")
+const getDirsDeepSync = require("../src/get-dirs-deep-sync.js")
+const config = require("./setup-and-teardown.js")
+const { sort, set } = require("@jrc03c/js-math-tools")
 
-test("", () => {})
+test("tests that directories can be gotten deeply", () => {
+  const results = sort(set(getDirsDeepSync(config.root).concat([config.root])))
+  expect(results).toStrictEqual(sort(set(config.dirs)))
+})
