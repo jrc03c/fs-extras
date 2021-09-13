7c9a6e7f7ec843e7dd5d6d556b1d689ac2597d60
jrc03c <jrc03c@pm.me>
2021-09-12 21:02:32 -0500

    ...

--- tests/get-dirs-deep-sync.test.js
+++ tests/get-dirs-deep-sync.test.js
@@ -2,12 +2,12 @@ const getDirsDeepSync = require("../src/get-dirs-deep-sync.js")
 const config = require("./setup-and-teardown.js")
 const { sort, set } = require("@jrc03c/js-math-tools")
 
-test("tests that directories can be gotten deeply", () => {
+test("tests that directories can be gotten deeply and synchronously", () => {
   const results = [config.root].concat(getDirsDeepSync(config.root))
   expect(results).toStrictEqual(sort(set(config.dirs)))
 })
 
-test("tests that directories can be gotten shallowly", () => {
+test("tests that directories can be gotten shallowly and synchronously", () => {
   const depth = 3
   const results = [config.root].concat(getDirsDeepSync(config.root, depth))
 
--- tests/get-dirs-deep.test.js
+++ tests/get-dirs-deep.test.js
@@ -1,3 +1,22 @@
-require("./setup-and-teardown.js")
+const getDirsDeep = require("../src/get-dirs-deep.js")
+const config = require("./setup-and-teardown.js")
+const { sort, set } = require("@jrc03c/js-math-tools")
 
-test("", () => {})
+test("tests that directories can be gotten deeply and asynchronously", async () => {
+  const results = [config.root].concat(await getDirsDeep(config.root))
+  expect(results).toStrictEqual(sort(set(config.dirs)))
+})
+
+test("tests that directories can be gotten shallowly and asynchronously", async () => {
+  const depth = 3
+  const results = [config.root].concat(await getDirsDeep(config.root, depth))
+
+  results.forEach(result => {
+    const parts = result
+      .replaceAll(config.root, "")
+      .split("/")
+      .filter(p => p.length > 0)
+
+    expect(parts.length).toBeLessThanOrEqual(depth)
+  })
+})
