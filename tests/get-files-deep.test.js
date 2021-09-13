aa4337d05a0465c9777b2e8759c188925db16dbf
jrc03c <jrc03c@pm.me>
2021-09-12 21:33:53 -0500

    ...

--- tests/get-files-deep.test.js
+++ tests/get-files-deep.test.js
@@ -1,3 +1,24 @@
-require("./setup-and-teardown.js")
+const getFilesDeep = require("../src/get-files-deep.js")
+const config = require("./setup-and-teardown.js")
+const { sort, set } = require("@jrc03c/js-math-tools")
 
-test("", () => {})
+test("tests that files can be gotten deeply and asynchronously", async () => {
+  const results = sort(set(await getFilesDeep(config.root)))
+  expect(results).toStrictEqual(sort(set(config.files)))
+})
+
+test("tests that files can be gotten shallowly and asynchronously", async () => {
+  const depth = 3
+  const resultsPred = await getFilesDeep(config.root, depth)
+
+  const resultsTrue = config.files.filter(file => {
+    const parts = file
+      .replaceAll(config.root, "")
+      .split("/")
+      .filter(p => p.length > 0)
+
+    return parts.length <= depth
+  })
+
+  expect(sort(set(resultsPred))).toStrictEqual(sort(set(resultsTrue)))
+})
