871640daaef85a1b7a4fb8256e3829c5e69369a5
jrc03c <jrc03c@pm.me>
2021-09-13 18:17:05 -0500

    ...

--- tests/get-dirs-deep.test.js
+++ tests/get-dirs-deep.test.js
@@ -3,20 +3,20 @@ const config = require("./setup-and-teardown.js")
 const { sort, set } = require("@jrc03c/js-math-tools")
 
 test("tests that directories can be gotten deeply and asynchronously", async () => {
-  // const results = [config.root].concat(await getDirsDeep(config.root))
-  // expect(results).toStrictEqual(sort(set(config.dirs)))
+  const results = [config.root].concat(await getDirsDeep(config.root))
+  expect(results).toStrictEqual(sort(set(config.dirs)))
 })
 
 test("tests that directories can be gotten shallowly and asynchronously", async () => {
-  // const depth = 3
-  // const results = [config.root].concat(await getDirsDeep(config.root, depth))
-  //
-  // results.forEach(result => {
-  //   const parts = result
-  //     .replaceAll(config.root, "")
-  //     .split("/")
-  //     .filter(p => p.length > 0)
-  //
-  //   expect(parts.length).toBeLessThanOrEqual(depth)
-  // })
+  const depth = 3
+  const results = [config.root].concat(await getDirsDeep(config.root, depth))
+
+  results.forEach(result => {
+    const parts = result
+      .replaceAll(config.root, "")
+      .split("/")
+      .filter(p => p.length > 0)
+
+    expect(parts.length).toBeLessThanOrEqual(depth)
+  })
 })
--- tests/get-files-deep.test.js
+++ tests/get-files-deep.test.js
@@ -3,22 +3,22 @@ const config = require("./setup-and-teardown.js")
 const { sort, set } = require("@jrc03c/js-math-tools")
 
 test("tests that files can be gotten deeply and asynchronously", async () => {
-  // const results = sort(set(await getFilesDeep(config.root)))
-  // expect(results).toStrictEqual(sort(set(config.files)))
+  const results = sort(set(await getFilesDeep(config.root)))
+  expect(results).toStrictEqual(sort(set(config.files)))
 })
 
 test("tests that files can be gotten shallowly and asynchronously", async () => {
-  // const depth = 3
-  // const resultsPred = await getFilesDeep(config.root, depth)
-  //
-  // const resultsTrue = config.files.filter(file => {
-  //   const parts = file
-  //     .replaceAll(config.root, "")
-  //     .split("/")
-  //     .filter(p => p.length > 0)
-  //
-  //   return parts.length <= depth
-  // })
-  //
-  // expect(sort(set(resultsPred))).toStrictEqual(sort(set(resultsTrue)))
+  const depth = 3
+  const resultsPred = await getFilesDeep(config.root, depth)
+
+  const resultsTrue = config.files.filter(file => {
+    const parts = file
+      .replaceAll(config.root, "")
+      .split("/")
+      .filter(p => p.length > 0)
+
+    return parts.length <= depth
+  })
+
+  expect(sort(set(resultsPred))).toStrictEqual(sort(set(resultsTrue)))
 })
