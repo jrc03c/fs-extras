17004cae7655d208e1ed51b875c12c3cd0b6fc85
jrc03c <jrc03c@pm.me>
2022-07-11 13:14:08 -0500

    ...

--- tests/copy-sync.test.js
+++ tests/copy-sync.test.js
@@ -1,6 +1,9 @@
-const { copySync } = require("..")
+const { copySync, getFilesDeepSync } = require("..")
+const { isEqual } = require("@jrc03c/js-math-tools")
 const config = require("./setup-and-teardown.js")
+const FileDB = require("@jrc03c/filedb")
 const fs = require("fs")
+const makeKey = require("./make-key.js")
 const path = require("path")
 
 test("tests that files can be copied synchronously", () => {
@@ -31,6 +34,25 @@ test("tests that files can be copied synchronously", () => {
   }
 })
 
-test("tests that directories can be copied synchronously", () => {})
+test("tests that directories can be copied synchronously", () => {
+  const src = config.dirs.random()
+
+  const dest = path.join(
+    config.root,
+    [makeKey(8), makeKey(8), makeKey(8)].join("/")
+  )
+
+  expect(fs.existsSync(src)).toBe(true)
+  expect(fs.existsSync(dest)).toBe(false)
+
+  copySync(src, dest)
+
+  expect(fs.existsSync(src)).toBe(true)
+  expect(fs.existsSync(dest)).toBe(true)
+
+  const srcObj = new FileDB(src).readSync("/")
+  const destObj = new FileDB(dest).readSync("/")
+  expect(isEqual(srcObj, destObj)).toBe(true)
+})
 
 test("tests that symlink targets are preserved when copied synchronously", () => {})
