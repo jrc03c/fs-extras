4780e09d403274fde9fa8f3bcae74efdcc561fe5
jrc03c <jrc03c@pm.me>
2022-07-11 11:38:24 -0500

    ...

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = {
-  findSync: require("./src/find-sync.js"),
+  copy: require("./src/copy.js"),
+  copySync: require("./src/copy-sync.js"),
   find: require("./src/find.js"),
-  getDirsDeepSync: require("./src/get-dirs-deep-sync.js"),
+  findSync: require("./src/find-sync.js"),
   getDirsDeep: require("./src/get-dirs-deep.js"),
-  getFilesDeepSync: require("./src/get-files-deep-sync.js"),
+  getDirsDeepSync: require("./src/get-dirs-deep-sync.js"),
   getFilesDeep: require("./src/get-files-deep.js"),
+  getFilesDeepSync: require("./src/get-files-deep-sync.js"),
+  move: require("./src/move.js"),
+  moveSync: require("./src/move-sync.js"),
 }
