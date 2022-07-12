85f591ac75cd6a1286188bf823ee232cd04c9348
jrc03c <jrc03c@pm.me>
2022-07-12 11:30:42 -0500

    ...

--- src/move-sync.js
+++ src/move-sync.js
@@ -1,32 +1,16 @@
+const copySync = require("./copy-sync.js")
 const fs = require("fs")
-const getFilesDeepSync = require("./get-files-deep-sync.js")
 
 function moveSync(src, dest) {
-  const stat = fs.lstatSync(src)
+  copySync(src, dest)
 
-  if (stat.isFile() || stat.isSymbolicLink()) {
-    const destParts = dest.split("/")
-    const destDir = destParts.slice(0, destParts.length - 1).join("/")
-
-    if (!fs.existsSync(destDir)) {
-      fs.mkdirSync(destDir, { recursive: true })
-    }
-
-    fs.renameSync(src, dest)
-    return true
+  if (fs.lstatSync(src).isDirectory()) {
+    fs.rmSync(src, { recursive: true, force: true })
   } else {
-    if (!fs.existsSync(dest)) {
-      fs.mkdirSync(dest, { recursive: true })
-    }
-
-    const children = getFilesDeepSync(src)
-
-    children.forEach(child => {
-      moveSync(child, child.replace(src, dest))
-    })
-
-    return true
+    fs.unlinkSync(src)
   }
+
+  return true
 }
 
 module.exports = moveSync
--- todo.md
+++ todo.md
@@ -1,1 +1,21 @@
-- decide when and how to follow symlinks
+# Symlinks
+
+Decide when and how to follow symlinks.
+
+# Moving / copying behavior
+
+Determine the expected behavior for copying and moving. For example, if I do this:
+
+```js
+copySync("/foo", "/bar/baz")
+```
+
+...then one of two behaviors might be expected:
+
+**Behavior #1:** The contents of "/foo" could be copied into "/bar/baz" (such that "/foo/myfile" would end up at "/bar/baz/myfile").
+
+**Behavior #2:** A new folder called "foo" could be created inside of "/bar/baz" and filled with the contents of "/foo" (such that "/foo/myfile" would end up at "/bar/baz/foo/myfile").
+
+Which behavior should users expect? And how should the behavior change (if at all) in situations where one argument is a file and the other is a directory?
+
+Presumably, users will expect whichever behavior matches the Bash `cp` behavior, so I probably need to incorporate that into the moving and copying tests!
