c5002fd3763282e123265a2ddbfae2168bd59aa7
jrc03c <jrc03c@pm.me>
2022-07-12 10:33:26 -0500

    ...

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -1,1 +1,37 @@
-test("", () => {})
+const { copySync } = require("..")
+const config = require("./setup-and-teardown.js")
+const fs = require("fs")
+const makeKey = require("./make-key.js")
+const path = require("path")
+
+test("tests that files can be copied asynchronously", async () => {
+  config.setup()
+
+  for (let i = 0; i < 100; i++) {
+    const src = config.files.random()
+    let destDir
+
+    while (!destDir || src.includes(destDir)) {
+      destDir = config.dirs.random()
+    }
+
+    const dest = path.join(destDir, makeKey(8))
+    expect(fs.existsSync(src)).toBe(true)
+    expect(fs.existsSync(dest)).toBe(false)
+
+    copySync(src, dest)
+
+    expect(fs.existsSync(src)).toBe(true)
+    expect(fs.existsSync(dest)).toBe(true)
+    expect(fs.readFileSync(dest, "utf8")).toBe(fs.readFileSync(src, "utf8"))
+
+    fs.unlinkSync(dest)
+  }
+
+  config.teardown()
+})
+
+test("tests that folders can be copied asynchronously", async () => {
+  config.setup()
+  config.teardown()
+})
