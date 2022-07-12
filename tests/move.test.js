def1f0731e06457c010c1a5286ab17f77ec202ad
jrc03c <jrc03c@pm.me>
2022-07-12 11:34:02 -0500

    ...

--- tests/move.test.js
+++ tests/move.test.js
@@ -1,1 +1,74 @@
-test("", () => {})
+const { move, getFilesDeep } = require("..")
+const config = require("./setup-and-teardown.js")
+const fs = require("fs")
+const makeKey = require("./make-key.js")
+const path = require("path")
+
+test("tests that files can be moved asynchronously", async () => {
+  config.setup()
+
+  for (let i = 0; i < 100; i++) {
+    const src = config.files.random()
+    const dest = path.join(config.dirs.random(), makeKey(8))
+
+    expect(fs.existsSync(src)).toBe(true)
+    expect(fs.existsSync(dest)).toBe(false)
+
+    await move(src, dest)
+
+    expect(fs.existsSync(src)).toBe(false)
+    expect(fs.existsSync(dest)).toBe(true)
+
+    config.files.splice(config.files.indexOf(src), 1)
+    config.files.push(dest)
+  }
+
+  config.teardown()
+})
+
+test("tests that directories can be moved asynchronously", async () => {
+  config.setup()
+  config.dirs.sort((a, b) => b.length - a.length)
+
+  const src = config.dirs[0]
+  let index = 0
+  let dest
+
+  while (
+    !dest ||
+    src.includes(dest) ||
+    dest.includes(src) ||
+    dest === config.root
+  ) {
+    dest = config.dirs[index]
+    index++
+  }
+
+  const originalSrcFiles = await getFilesDeep(src)
+  const originalDestFiles = await getFilesDeep(dest)
+
+  await move(src, dest)
+
+  const srcFiles = originalSrcFiles.map(f => f.replace(src, "")).sort()
+
+  const destFiles = (await getFilesDeep(dest))
+    .filter(f => originalDestFiles.indexOf(f) < 0)
+    .map(f => f.replace(dest, ""))
+    .sort()
+
+  expect(srcFiles).toStrictEqual(destFiles)
+  expect(fs.existsSync(src)).toBe(false)
+  config.teardown()
+})
+
+test("tests that symlink targets are preserved when moved asynchronously", () => {
+  // console.warn("Remember to build this test!")
+})
+
+test("tests that preexisting files are overwritten during an asynchronous move", () => {
+  // console.warn("Remember to build this test!")
+})
+
+test("tests that empty directories can be moved asynchronously", () => {
+  // console.warn("Remember to build this test!")
+})
