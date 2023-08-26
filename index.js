c1105117a6ad3167ce06f10ca5402db95861ec29
jrc03c <jrc03c@pm.me>
2023-08-26 12:26:51 -0500

    ...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
+  createFileStreamReader: require("./src/create-file-stream-reader.js"),
+  createFileStreamWriter: require("./src/create-file-stream-writer.js"),
   findSync: require("./src/find-sync.js"),
   find: require("./src/find.js"),
   getDirsDeepSync: require("./src/get-dirs-deep-sync.js"),
